8bae6cbfcf4e242bbe6faa0db0a90d71d2f537f3
grakzanchetta <grakzanchetta@gmail.com>
2022-04-21 16:33:08 -0300

    correção de erros

--- scripts/quizzListing.js
+++ scripts/quizzListing.js
@@ -20,26 +20,40 @@ function renderizarQuizzes(infoResposta){
         let titulo = infoResposta[i].title;
         let imagem = infoResposta[i].image;
 
-    list.innerHTML += `<div class="quiz" onclick="quizAnswer(${id})">
+    list.innerHTML += `<div class="quiz" onclick="quizToPlay(${id})">
                         <img class="quiz-image" src="${imagem}"/>
                         <img class="black-mask" src="/blackmask1.jpg"/>
                         <div class="quiz-title">${titulo}</div>
-                        </div>`        
+                        </div>`    
     }
-    console.log(infoResposta)
 }
 
-function quizAnswer(id){
+function quizToPlay(id){
+    
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${id}`);
+    promise.then((resposta) => { 
+        console.log(resposta.data);
+    });
+
+
+    document.querySelector(".container-pagina-1").classList.add("hidden");
+    document.querySelector(".container-pagina-2").classList.remove("hidden");
+
+}
+
+
+
+
+
+
+
+/* function quizAnswer(){
     const promise = axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${id}`);
     promise.then((resposta) => { 
         renderizarQuiz(resposta.data);
     });
 }
 
-
-
 function renderizarQuiz (infoQuiz){
-    console.log(infoQuiz);
-    document.querySelector(".container-pagina-1").classList.add("hidden");
-    document.querySelector(".container-pagina-2").classList.remove("hidden");
-}
+
+} */
