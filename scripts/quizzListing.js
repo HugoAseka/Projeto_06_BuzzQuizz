ddb9f0bcb16d8cca1790e1f390433e6feb75789e
grakzanchetta <grakzanchetta@gmail.com>
2022-04-21 19:02:29 -0300

    push

--- scripts/quizzListing.js
+++ scripts/quizzListing.js
@@ -32,28 +32,38 @@ function quizToPlay(id){
     
     const promise = axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${id}`);
     promise.then((resposta) => { 
-        console.log(resposta.data);
+        renderizarQuizz(resposta.data);
     });
-
 
     document.querySelector(".container-pagina-1").classList.add("hidden");
     document.querySelector(".container-pagina-2").classList.remove("hidden");
 
 }
 
+function renderizarQuizz(infoResposta){
+    console.log(infoResposta);
+
+    const banner = document.querySelector(".banner");
+    let imagem = infoResposta.image;
+    let titulo = infoResposta.title;
+
+    banner.innerHTML = `<img class="main-image" src='${imagem}'/><div class="main-quiz-title">${titulo}</div>`
+
+   
+    const question = document.querySelector(".question-list")
+    const answerBox = document.querySelector(".answer-container")
+
+    for (let i = 0; i < infoResposta.questions.length; i++){
+        question.innerHTML += `<div class="question-main"><div class="question-main-title" style="background-color: ${infoResposta.questions[i].color};"><div class="question-title">${infoResposta.questions[i].title}</div></div></div>`
+                                                         
+
+        }
+
+
+
+    }    
 
 
 
 
 
-
-/* function quizAnswer(){
-    const promise = axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${id}`);
-    promise.then((resposta) => { 
-        renderizarQuiz(resposta.data);
-    });
-}
-
-function renderizarQuiz (infoQuiz){
-
-} */
