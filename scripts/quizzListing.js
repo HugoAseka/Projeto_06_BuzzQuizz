1a9cc275b086b2e591909b043244afb616887a75
grakzanchetta <grakzanchetta@gmail.com>
2022-04-23 21:08:03 -0300

    inicio do comportamento das respostas. Layout precisa ser (re)consertado

--- scripts/quizzListing.js
+++ scripts/quizzListing.js
@@ -1,16 +1,8 @@
-/* A minha ideia pra renderizar a lista de perguntas é 1) Usar o inner.HTML pra renderizar bloco a bloco, baseado no tamanho do vetor de questões. Essa parte está ok, porém cada questão tem um numero X de perguntas, de 2 a 4, a qual eu queria renderizar da mesma forma.
-
-A ideia seria uma iteração renderiza o bloco da pergunta, em seguida outra iteração renderiza as respostas em si. Passa pra 2a iteração de bloco, mais uma iteração das respostas... e assim segue até renderizar a lista toda*/
-
-
-
-
-
-
 function createNewQuiz(){
     document.querySelector(".container-pagina-1").classList.add("hidden");
     document.querySelector(".creation").classList.remove("hidden");
 }
+
 
 const promise = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
 promise.then((resposta) => { 
@@ -30,38 +22,32 @@ function renderizarQuizzes(infoResposta){
         let imagem = infoResposta[i].image;
 
     list.innerHTML += `<div class="quiz" onclick="quizToPlay(${id})">
-                        <img class="quiz-image" src="${imagem}"/>
-                        <img class="black-mask" src="/blackmask1.jpg"/>
-                        <div class="quiz-title">${titulo}</div>
+                            <img class="quiz-image" src="${imagem}"/>
+                            <img class="black-mask" src="/blackmask1.jpg"/>
+                            <div class="quiz-title">${titulo}
+                            </div>
                         </div>`    
     }
 }
 
 function quizToPlay(id){
-    
     const promise = axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${id}`);
     promise.then((resposta) => { 
        randomizeQuizz(resposta.data);
         renderizarQuizz(resposta.data);
     });
-
     document.querySelector(".container-pagina-1").classList.add("hidden");
     document.querySelector(".container-pagina-2").classList.remove("hidden");
-
 }
 
-
 function randomizeQuizz (infoResposta){
-
-    for (let i = 0; i < infoResposta.questions.length; i++){
-        
-       let shufflin = infoResposta.questions[i].answers.sort( () => .5 - Math.random() );
-        console.log(infoResposta.questions[i].answers);      
+    for (let i = 0; i < infoResposta.questions.length; i++){    
+       let shufflin = infoResposta.questions[i].answers.sort( () => .5 - Math.random() );   
     }
-} 
+}
 
 function renderizarQuizz(infoResposta){
-   
+    
     let imagem = infoResposta.image;
     let titulo = infoResposta.title;
 
@@ -69,16 +55,28 @@ function renderizarQuizz(infoResposta){
     banner.innerHTML = `<img class="main-image" src='${imagem}'/><div class="main-quiz-title">${titulo}</div>`
 
     let questionBox = document.querySelector(".question-list");
-
-
     for (let i = 0; i < infoResposta.questions.length; i++){
-        
-            questionBox.innerHTML += ` <div class="question-main">
-                                            <div class="question-main-title" style="background-color: ${infoResposta.questions[i].color};"><div class="question-title">${infoResposta.questions[i].title}</div></div>
-                    <div class="answer-container"><div class='answers-box' id='${i+1}'>`
-        
-                    for (let k = 0; k <infoResposta.questions[i].answers.length; k++){
+        questionBox.innerHTML += ` <div class="question-main">
+                                        <div class="question-main-title" style="background-color: ${infoResposta.questions[i].color};">
+                                            <div class="question-title">${infoResposta.questions[i].title}
+                                            </div>
+                                        </div>
+                                        <div class="answer-container">
+                                        <div class='answers-box' id='${i+1}'>`        
+        for (let k = 0; k <infoResposta.questions[i].answers.length; k++){
             let answerBox = document.querySelector(`#${CSS.escape(i+1)}`);
-            answerBox.innerHTML += `<div class="answer-unique" id='${k}'><img class="answer-unique-image" src='${infoResposta.questions[i].answers[k].image}'><div class="answer-unique-text">${infoResposta.questions[i].answers[k].text}</div></div>` } questionBox.innerHTML += `</div></div></div>`;
-    }  
+                answerBox.innerHTML += `<div class="answer-unique" id='${k}'>
+                                        <img class="answer-unique-image" id='${i}${infoResposta.questions[i].answers[k].isCorrectAnswer}'  onclick="answerQuizz(this)" src='${infoResposta.questions[i].answers[k].image}'>
+                                        <div class="answer-unique-text">${infoResposta.questions[i].answers[k].text}
+                                    </div>
+                                </div>`
+                            } 
+                questionBox.innerHTML += `</div></div></div>`;            
+            }  
+}                                                                                                                                                                                                                                               
+function answerQuizz (click){
+    console.log(click.id[0]);
+    if (click.id[1] === 'f'){
+        console.log("Resposta Errada");
+    }
 }
